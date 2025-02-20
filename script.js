1098b1908e5b73c27e94b3c2da488773b1ffdee5
Adin Karic <adin.karic.07@gmail.com>
2025-02-20 15:40:01 +0100

    Details Modal (mit pos: absolute, andere Möglichkeit mit grid oder flex; zu besprechen)

--- script.js
+++ script.js
@@ -1,31 +1,61 @@
-const modalData = {
+const objectData = {
     blue: {
         "title": "Blue",
-        "text": "Lorem Ipsum"
+        "modalText": "Lorem Ipsum",
+        "detailsText": "Lorem Ipsum 1"
     },
     green: {
         "title": "Green",
-        "text": "Lorem Ipsum"
+        "modalText": "Lorem Ipsum",
+        "detailsText": "Lorem Ipsum 1"
     },
     yellow: {
         "title": "Yellow",
-        "text": "Lorem Ipsum"
+        "modalText": "Lorem Ipsum",
+        "detailsText": "Lorem Ipsum 1"
     }
 };
+var currentObject = null;
 
 function openModal(objectID) {
+    currentObject = objectID;
     let modal = document.getElementById("modal");
     let backdrop = document.getElementById("backdrop");
-    modal.children[1].textContent = modalData[objectID].title;
-    modal.children[2].textContent = modalData[objectID].text;
+    modal.children[1].textContent = objectData[objectID].title;
+    modal.children[2].textContent = objectData[objectID].modalText;
     backdrop.style.display = "block"
     modal.showPopover();
+
 }
-function closeModal() {
+function closeModal(inclBackdrop) {
     let modal = document.getElementById("modal");
     let backdrop = document.getElementById("backdrop");
     modal.hidePopover();
-    backdrop.style.display = "none"
+    if(inclBackdrop) { backdrop.style.display = "none"; }
+}
+
+function openDetails() {
+    let details = document.getElementById("details");
+    let backdrop = document.getElementById("backdrop");
+    let object = document.getElementById(currentObject);
+    details.children[1].textContent = objectData[currentObject].title;
+    details.children[2].textContent = objectData[currentObject].detailsText;
+    closeModal(false);
+    object.classList.remove("liftable");
+    object.classList.add("fullscreen");
+    details.showPopover();
+    backdrop.classList.add("transparent");
+}
+
+function closeDetails() {
+    let details = document.getElementById("details");
+    let backdrop = document.getElementById("backdrop");
+    let object = document.getElementById(currentObject);
+    object.classList.remove("fullscreen");
+    object.classList.add("liftable");
+    details.hidePopover();
+    backdrop.style.display = "none";
+    backdrop.classList.remove("transparent");
 }
 
 window.addEventListener("load", () => {
